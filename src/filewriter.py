dde3a1925175a5640eec445d9cf2a59bd412bebf
Siyuan Chen <daniel@apextone.com>
2020-06-14 20:19:33 -0400

    Added pylatex integration for filewriter.
    Note

--- src/filewriter.py
+++ src/filewriter.py
@@ -1,5 +1,5 @@
 from pdflatex import PDFLaTeX
-from pylatex import Document, Section, Command
+from pylatex import Document, Section, Subsection, Command
 from pylatex.utils import NoEscape
 
 
@@ -31,11 +31,28 @@ class Filewriter:
         pdfl.set_jobname(pdf_filename)
         pdfl.create_pdf(keep_pdf_file=True)
 
+    def __init__(self, tex_filename):
+        self.doc = Document(tex_filename)
 
-'''doc = Document('test')
-doc.preamble.append(Command('title', 'TITLE'))
-doc.preamble.append(Command('author', 'Daniel Chen'))
-doc.preamble.append(Command('date', r'\today'))
-doc.generate_tex()
-'''
-Filewriter.write_pdf_from_tex('test.tex', 'test3.pdf')
+    def write_preamble(self, title: str, author: str, with_date: bool = True):
+        self.doc.preamble.append(Command('title', title))
+        self.doc.preamble.append(Command('author', author))
+        if with_date:
+            self.doc.preamble.append(Command('date', NoEscape(r'\today')))
+        self.doc.append(NoEscape(r'\maketitle'))
+
+    def write_section(self, section_name: str, content: str):
+        with self.doc.create(Section(section_name)):
+            self.doc.append(content)
+
+    def write_tex(self):
+        self.doc.generate_tex()
+
+
+f = Filewriter('hello')
+f.write_preamble('Problem set', 'Author', True)
+f.write_section('section 1', 'Something somethings')
+f.write_section('section 3', 'Sometsdf')
+f.write_tex()
+
+Filewriter.write_pdf_from_tex('hello.tex')
