c7d5bd2732934e0228b361eb2dc9f436fb4fd369
Siyuan Chen <daniel@apextone.com>
2020-06-16 15:48:47 -0400

    Added problem description individually.
    Note

--- src/main.py
+++ src/main.py
@@ -3,19 +3,22 @@ from extractor import Extractor
 from filewriter import Filewriter
 
 
-def get_problem(exam: str, year: int, problem: int) -> str:
-    return Extractor.extract_question(Generator.generate_url(exam, year, problem))
+def get_problem(exam: str, year: int, problem: int, description: bool = False) -> str:
+    return (('[\\textbf{{{} {} P{}}}] '.format(year, exam.replace('_', ' '), problem) if description else '')
+            + Extractor.extract_question(Generator.generate_url(exam, year, problem)))
 
 
 def main():
-    filewriter = Filewriter('output/2020_AIME_II')
+    filewriter = Filewriter('output/2018_AIME_II')
 
-    filewriter.initialize_doc('2020 AIME II Problems', 'MAA')
+    filewriter.initialize_doc('2018 AIME II Problems', 'MAA')
     for i in range(15):
-        filewriter.add_problem_to_section(get_problem('AIME_II', 2020, i + 1))
+        filewriter.add_problem_to_section(get_problem('AIME_II', 2018, i + 1, description=True))
         print('Problem {} successfully added to tex file'.format(i + 1))
     filewriter.add_section('Problems')
     filewriter.write_tex()
+
+    Filewriter.write_pdf_from_tex('output/2018_AIME_II.tex')
 
 
 if __name__ == '__main__':
